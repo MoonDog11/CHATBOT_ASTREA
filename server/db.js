e44661ce3e74b8d8cf88be95cee2b73af96bdc77
MoonDog11 <123662244+MoonDog11@users.noreply.github.com>
2024-08-18 17:50:27 -0500

    Update db.js

--- server/db.js
+++ server/db.js
@@ -8,7 +8,7 @@ const pool = new Pool({
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
-  connectionString: process.env.DATABASE_URL // Usar 'connectionString' en lugar de 'url'
+  connectionString: process.env.URL_DATABASE // Usar 'connectionString' en lugar de 'url'
 });
 
 // Agregar un console.log para verificar la conexión
