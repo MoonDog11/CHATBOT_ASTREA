6f3f9e60b56598ae7d7ae1f61b61d34ac9ab9062
MoonDog11 <123662244+MoonDog11@users.noreply.github.com>
2024-08-17 20:30:31 -0500

    Update app.js

--- server/app.js
+++ server/app.js
@@ -40,6 +40,13 @@ pool.on('error', (err, client) => {
   process.exit(-1);
 });
 
+console.log('Variables de Entorno:');
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_DATABASE:', process.env.DB_DATABASE);
+console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+console.log('DB_PORT:', process.env.DB_PORT);
+console.log('DATABASE_URL:', process.env.DATABASE_URL);
 
 // Middleware
 app.use(
