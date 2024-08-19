30c0bbc2a6acf6c80c6fe19b548e3a33162eb26b
MoonDog11 <123662244+MoonDog11@users.noreply.github.com>
2024-08-19 18:11:38 -0500

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,43 +1,45 @@
-const { Pool } = require('pg');
+// db.js
 require('dotenv').config();
+const { Pool } = require('pg');
 
-// Configurar la conexión a la base de datos
+// Configuración del Pool
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-  connectionString: process.env.DATABASE_URL // Usar 'connectionString' en lugar de 'url'
+  user: process.env.DB_USER || undefined,
+  host: process.env.DB_HOST || undefined,
+  database: process.env.DB_DATABASE || undefined,
+  password: process.env.DB_PASSWORD || undefined,
+  port: process.env.DB_PORT || undefined,
+  connectionString: process.env.DATABASE_URL || undefined, // Prioriza DATABASE_URL si está presente
+  ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false // Habilita SSL solo si se usa DATABASE_URL
 });
 
-// Agregar un console.log para verificar la conexión
-pool.connect((err, client, release) => {
-  if (err) {
+// Función para verificar la conexión y consultar la versión del servidor
+const checkConnection = async () => {
+  try {
+    const client = await pool.connect();
+    console.log('Conexión exitosa a la base de datos');
+    console.log('Usuario conectado:', client.user); // Añade el usuario conectado
+    console.log('Versión del servidor PostgreSQL:', client.serverVersion); // Añade la versión del servidor
+    client.release();
+
+    const { rows } = await pool.query('SELECT version()');
+    console.log('Versión del servidor PostgreSQL:', rows[0].version);
+
+  } catch (err) {
     console.error('Error al conectar a la base de datos:', err.message);
-    return;
+  } finally {
+    await pool.end();
   }
-  console.log('Conexión exitosa a la base de datos');
-  console.log('Usuario conectado:', client.user); // Añade el usuario conectado
-  console.log('Versión del servidor PostgreSQL:', client.serverVersion); // Añade la versión del servidor
-  release();
-});
+};
 
-
+// Verificar si la variable DATABASE_URL está configurada
 const urlDatabase = process.env.DATABASE_URL;
 
-// Verificar si la variable está configurada y tiene un valor
 if (urlDatabase) {
   console.log(`DATABASE_URL configurada correctamente: ${urlDatabase}`);
 } else {
   console.log('DATABASE_URL no está configurada correctamente o está vacía.');
 }
 
-pool.query('SELECT version()', (err, res) => {
-  if (err) {
-    console.error('Error al consultar la versión del servidor:', err.message);
-  } else {
-    console.log('Versión del servidor PostgreSQL:', res.rows[0].version);
-  }
-  pool.end();
-});
+// Ejecutar la verificación de conexión
+checkConnection();
