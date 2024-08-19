4d2bf0540048fa1ad8d4d09bdf195ec67178f77e
MoonDog11 <123662244+MoonDog11@users.noreply.github.com>
2024-08-19 18:37:28 -0500

    Update db.js

--- server/db.js
+++ server/db.js
@@ -4,30 +4,31 @@ const { Pool } = require('pg');
 
 // Configuración del Pool
 const pool = new Pool({
-  user: process.env.DB_USER || undefined,
-  host: process.env.DB_HOST || undefined,
-  database: process.env.DB_DATABASE || undefined,
-  password: process.env.DB_PASSWORD || undefined,
-  port: process.env.DB_PORT || undefined,
-  connectionString: process.env.DATABASE_URL || undefined, // Prioriza DATABASE_URL si está presente
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
   ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false // Habilita SSL solo si se usa DATABASE_URL
 });
 
 // Función para verificar la conexión y consultar la versión del servidor
 const checkConnection = async () => {
+  let client;
   try {
-    const client = await pool.connect();
+    client = await pool.connect();
     console.log('Conexión exitosa a la base de datos');
-    console.log('Usuario conectado:', client.user); // Añade el usuario conectado
-    console.log('Versión del servidor PostgreSQL:', client.serverVersion); // Añade la versión del servidor
-    client.release();
-
+    
+    // Consultar la versión del servidor PostgreSQL
     const { rows } = await pool.query('SELECT version()');
     console.log('Versión del servidor PostgreSQL:', rows[0].version);
 
   } catch (err) {
     console.error('Error al conectar a la base de datos:', err.message);
   } finally {
+    // Liberar el cliente y cerrar el pool
+    if (client) client.release();
     await pool.end();
   }
 };
