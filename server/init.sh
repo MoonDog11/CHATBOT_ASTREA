f56db8381421e91416dd8dc6fd0c1bb90c791caa
MoonDog11 <123662244+MoonDog11@users.noreply.github.com>
2024-08-21 13:57:50 -0500

    Update init.sh

--- server/init.sh
+++ server/init.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-# Asegurarse de que migrate.sh tenga permisos de ejecución
+# Asegurar que migrate.sh tenga permisos de ejecución
 chmod +x /app/migrate.sh
 
-# Ejecutar el script de migración y luego el servidor
+# Ejecutar el script de migración
 /app/migrate.sh
+
+# Copiar landing.html si está presente en /app/client
+if [ -f "/app/client/landing.html" ]; then
+    cp /app/client/landing.html /app/
+fi
+
+# Iniciar el servidor Node.js
 node /app/app.js
+
