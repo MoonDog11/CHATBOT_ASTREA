c3987582727433859fc4298eff3786cb0420a543
MoonDog11 <123662244+MoonDog11@users.noreply.github.com>
2024-08-23 19:16:25 -0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 
-# Asegurar que migrate.sh tenga permisos de ejecución
+# Asegurarse de que migrate.sh tenga permisos de ejecución
 chmod +x /app/migrate.sh
 
 # Verificar la existencia del archivo migrate.sh
 if [ -f /app/migrate.sh ]; then
     echo "migrate.sh encontrado."
+    # Ejecutar el script de migración
+    /app/migrate.sh
 else
     echo "migrate.sh no encontrado."
     exit 1
 fi
 
-# Ejecutar el script de migración
-/app/migrate.sh
-
-# Iniciar el servidor Node.js
-node /app/app.js
-
-
-
+# Iniciar el servidor Node.js desde el directorio server
+node /app/server/app.js
