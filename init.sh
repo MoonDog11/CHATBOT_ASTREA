a8941d50869b16eeb23922f24fffb21eb348b233
MoonDog11 <123662244+MoonDog11@users.noreply.github.com>
2024-08-25 17:10:16 -0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,12 +1,17 @@
-# Ejecutar migraciones de base de datos si existe el archivo /app/migrate.sh
+#!/bin/bash
+
+# Imprimir un mensaje indicando el inicio de la inicialización
+echo "Starting initialization..."
+
+# Ejecutar migraciones de base de datos si existe el archivo /app/server/migrate.sh
 echo "Running database migrations..."
-if [ -f /app/migrate.sh ]; then
-    /app/migrate.sh
+if [ -f /app/server/migrate.sh ]; then
+    /app/server/migrate.sh
 else
     echo "Migration script not found!"
 fi
 
-# Iniciar la aplicación Node.js (app.js) si existe el archivo /app/server/app.js
+# Iniciar la aplicación Node.js (app.js) desde el directorio /app/server
 echo "Starting the application..."
 if [ -f /app/server/app.js ]; then
     node /app/server/app.js
