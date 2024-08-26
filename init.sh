5104acb93e2eb5cb89aa6ec1c987ea9ec64914e0
MoonDog11 <123662244+MoonDog11@users.noreply.github.com>
2024-08-26 13:01:12 -0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -11,16 +11,28 @@ else
     echo "Migration script not found!"
 fi
 
-# Cambiar al directorio /app/server para iniciar la aplicación Node.js
+# Cambiar al directorio /server para iniciar la aplicación Node.js
 cd /server || exit
 
-# Iniciar la aplicación Node.js (app.js) desde el directorio /app/server
+# Iniciar la aplicación Node.js (app.js) desde el directorio /server
 echo "Starting the application..."
 if [ -f /server/app.js ]; then
-    node app.js
+    node app.js &
 else
     echo "Application entry point not found!"
+    exit 1
 fi
+
+# Esperar a que la aplicación se inicie correctamente
+echo "Waiting for the application to start..."
+while ! nc -z localhost 8080; do
+  sleep 0.1
+done
+
+echo "Application started successfully."
 
 # Imprimir un mensaje indicando la finalización de la inicialización
 echo "Initialization complete."
+
+# Mantener el contenedor en ejecución
+tail -f /dev/null
