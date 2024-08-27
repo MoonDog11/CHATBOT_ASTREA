ba04435010a9c3145a5153a88240f08c027373c1
MoonDog11 <123662244+MoonDog11@users.noreply.github.com>
2024-08-27 11:21:10 -0500

    Update migrate.sh

--- migrate.sh
+++ migrate.sh
@@ -2,16 +2,14 @@
 
 set -o pipefail
 
-
 # Definición de colores para salida en consola
-_TERM=ansi
-_GREEN=$(tput setaf 2)
-_BLUE=$(tput setaf 4)
-_MAGENTA=$(tput setaf 5)
-_RESET=$(tput sgr0)
-_BOLD=$(tput bold)
-_RED=$(tput setaf 1)
-_YELLOW=$(tput setaf 3)
+_GREEN="\033[0;32m"
+_BLUE="\033[0;34m"
+_MAGENTA="\033[0;35m"
+_RESET="\033[0m"
+_BOLD="\033[1m"
+_RED="\033[0;31m"
+_YELLOW="\033[1;33m"
 
 # Función para mostrar errores y salir
 error_exit() {
@@ -67,17 +65,17 @@ fi
 
 write_ok "DATABASE_URL correctly set"
 
-# Extraer información de DATABASE_URL
-export DB_HOST=$(echo $DATABASE_URL | sed -n 's/.*@\(.*\):[0-9]*\/.*/\1/p')
-export DB_PORT=$(echo $DATABASE_URL | sed -n 's/.*:[0-9]*\/.*/56284/p')
-export DB_USER=$(echo $DATABASE_URL | sed -n 's/.*\/\/\([^:]*\):.*/\1/p')
-export DB_PASSWORD=$(echo $DATABASE_URL | sed -n 's/.*:\([^@]*\)@.*/\1/p')
-export DB_NAME=$(echo $DATABASE_URL | sed -n 's/.*\/\([^?]*\).*/\1/p')
+# Extraer información de DATABASE_URL usando bash
+export DB_HOST=$(echo "$DATABASE_URL" | awk -F[@:] '{print $4}')
+export DB_PORT=$(echo "$DATABASE_URL" | awk -F[@:] '{print $5}')
+export DB_USER=$(echo "$DATABASE_URL" | awk -F[/@] '{print $4}')
+export DB_PASSWORD=$(echo "$DATABASE_URL" | awk -F[:] '{print $3}' | sed 's/@.*//')
+export DB_NAME=$(echo "$DATABASE_URL" | awk -F[/:] '{print $4}')
 
 section "Checking if DATABASE_URL is empty"
 
 query="SELECT count(*) FROM information_schema.tables WHERE table_schema NOT IN ('information_schema', 'pg_catalog');"
-table_count=$(PGPASSWORD=$DB_PASSWORD psql -h $DB_HOST -U $DB_USER -p $DB_PORT -d "$DB_NAME" -t -A -c "$query")
+table_count=$(PGPASSWORD="$DB_PASSWORD" psql -h "$DB_HOST" -U "$DB_USER" -p "$DB_PORT" -d "$DB_NAME" -t -A -c "$query")
 
 if [[ $table_count -eq 0 ]]; then
   write_ok "The new database is empty. Proceeding with restore."
@@ -90,28 +88,28 @@ else
 fi
 
 dump_dir="plugin_dump"
-mkdir -p $dump_dir
+mkdir -p "$dump_dir"
 
 # Función para volcar la base de datos
 dump_database() {
-  local database=$1
+  local database="$1"
   local dump_file="$dump_dir/$database.sql"
 
   section "Dumping database: $database"
 
-  local base_url=$(echo $PLUGIN_URL | sed -E 's/(postgresql:\/\/[^:]+:[^@]+@[^:]+:[0-9]+)\/.*/\1/')
+  local base_url=$(echo "$PLUGIN_URL" | sed -E 's/(postgresql:\/\/[^:]+:[^@]+@[^:]+:[0-9]+)\/.*/\1/')
   local db_url="${base_url}/${database}"
 
   echo "Dumping database from $db_url"
 
-  PGPASSWORD=$DB_PASSWORD pg_dump -h $DB_HOST -U $DB_USER -p $DB_PORT -d "$database" \
+  PGPASSWORD="$DB_PASSWORD" pg_dump -h "$DB_HOST" -U "$DB_USER" -p "$DB_PORT" -d "$database" \
       --format=plain \
       --quote-all-identifiers \
       --no-tablespaces \
       --no-owner \
       --no-privileges \
       --disable-triggers \
-      --file=$dump_file || error_exit "Failed to dump database from $database."
+      --file="$dump_file" || error_exit "Failed to dump database from $database."
 
   write_ok "Successfully saved dump to $dump_file"
 
@@ -120,7 +118,7 @@ dump_database() {
 }
 
 # Obtener listado de bases de datos a migrar
-databases=$(PGPASSWORD=$DB_PASSWORD psql -h $DB_HOST -U $DB_USER -p $DB_PORT -d "$DB_NAME" -t -A -c "SELECT datname FROM pg_database WHERE datistemplate = false;")
+databases=$(PGPASSWORD="$DB_PASSWORD" psql -h "$DB_HOST" -U "$DB_USER" -p "$DB_PORT" -d "$DB_NAME" -t -A -c "SELECT datname FROM pg_database WHERE datistemplate = false;")
 write_info "Found databases to migrate: $databases"
 
 # Iterar sobre cada base de datos para realizar el volcado
@@ -130,25 +128,27 @@ done
 
 # Función para restaurar la base de datos desde el volcado
 restore_database() {
-  section "Restoring database: $db"
+  local database="$1"
 
-  local base_url=$(echo $DATABASE_URL | sed -E 's/(postgresql:\/\/[^:]+:[^@]+@[^:]+:[0-9]+)\/.*/\1/')
-  local db_url="${base_url}/${db}"
+  section "Restoring database: $database"
 
-  local db_name=$(echo $db_url | sed -E 's/.*\/([^\/?]+).*/\1/')
-  local db_url_base=$(echo $db_url | sed -E 's/(.*)\/[^\/?]+/\1/')
+  local base_url=$(echo "$DATABASE_URL" | sed -E 's/(postgresql:\/\/[^:]+:[^@]+@[^:]+:[0-9]+)\/.*/\1/')
+  local db_url="${base_url}/${database}"
 
-  if ! PGPASSWORD=$DB_PASSWORD psql -h $DB_HOST -U $DB_USER -p $DB_PORT -d $db_url_base -tA -c "SELECT 1 FROM pg_database WHERE datname='$db_name'" | grep -q 1; then
+  local db_name=$(echo "$db_url" | sed -E 's/.*\/([^?]+).*/\1/')
+  local db_url_base=$(echo "$db_url" | sed -E 's/(.*)\/[^\/?]+/\1/')
+
+  if ! PGPASSWORD="$DB_PASSWORD" psql -h "$DB_HOST" -U "$DB_USER" -p "$DB_PORT" -d "$db_url_base" -tA -c "SELECT 1 FROM pg_database WHERE datname='$db_name'" | grep -q 1; then
       write_ok "Database $db_name does not exist. Creating..."
-      PGPASSWORD=$DB_PASSWORD psql -h $DB_HOST -U $DB_USER -p $DB_PORT -d $db_url_base -c "CREATE DATABASE \"$db_name\""
+      PGPASSWORD="$DB_PASSWORD" psql -h "$DB_HOST" -U "$DB_USER" -p "$DB_PORT" -d "$db_url_base" -c "CREATE DATABASE \"$db_name\""
   else
       write_info "Database $db_name exists."
   fi
 
-  PGPASSWORD=$DB_PASSWORD psql -h $DB_HOST -U $DB_USER -p $DB_PORT -d $db_url -v ON_ERROR_STOP=1 --echo-errors \
-    -f "$dump_dir/$db.sql" || error_exit "Failed to restore database $db."
+  PGPASSWORD="$DB_PASSWORD" psql -h "$DB_HOST" -U "$DB_USER" -p "$DB_PORT" -d "$db_url" -v ON_ERROR_STOP=1 --echo-errors \
+    -f "$dump_dir/$database.sql" || error_exit "Failed to restore database $database."
   
-  write_ok "Successfully restored database $db from dump"
+  write_ok "Successfully restored database $database from dump"
 }
 
 # Iterar sobre cada base de datos para restaurarla desde el volcado
@@ -160,5 +160,5 @@ echo "Migration completed successfully."
 
 # Iniciar el servidor (asumiendo que esta parte es para otro script o aplicación relacionada)
 section "Starting the server"
-node app.js || error_exit "Failed to start the server."
+# node app.js || error_exit "Failed to start the server."
 write_ok "Server started successfully."
