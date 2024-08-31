1542ed5d69b0006fbf3aa2fdb12049647574eddc
MoonDog11 <123662244+MoonDog11@users.noreply.github.com>
2024-08-30 19:52:44 -0500

    Update db.js

--- server/db.js
+++ server/db.js
@@ -2,43 +2,25 @@
 require('dotenv').config();
 const { Pool } = require('pg');
 
-// Configuración del Pool
 const pool = new Pool({
-  user: 'postgres',
-  host: 'viaduct.proxy.rlwy.net',
-  database: 'railway',
-  password: 'RoJuKhWPvLtbSQILdwueQPcKMGUuXMkE',
-  port: 56284,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
 });
 
-// Función para verificar la conexión y consultar la versión del servidor
-const checkConnection = async () => {
-  let client;
+const testQuery = async () => {
   try {
-    client = await pool.connect();
-    console.log('Conexión exitosa a la base de datos');
-    
-    // Consultar la versión del servidor PostgreSQL
-    const { rows } = await pool.query('SELECT version()');
-    console.log('Versión del servidor PostgreSQL:', rows[0].version);
-
+    const res = await pool.query('SELECT NOW()');
+    console.log('Hora actual en la base de datos:', res.rows[0].now);
   } catch (err) {
-    console.error('Error al conectar a la base de datos:', err.message);
-  } finally {
-    // Liberar el cliente y cerrar el pool
-    if (client) client.release();
-    await pool.end();
+    console.error('Error en la consulta de prueba:', err);
   }
 };
 
-// Verificar si la variable DATABASE_URL está configurada
-const urlDatabase = process.env.DATABASE_URL;
+testQuery();
 
-if (urlDatabase) {
- console.log(`DATABASE_URL configurada correctamente: ${urlDatabase}`);
-} else {
-  console.log('DATABASE_URL no está configurada correctamente o está vacía.');
-}
-
-// Ejecutar la verificación de conexión
-checkConnection()
+module.exports = { pool };
