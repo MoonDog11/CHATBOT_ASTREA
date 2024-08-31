5c155c267576d9150f721b1987c58c5848cc16e1
MoonDog11 <123662244+MoonDog11@users.noreply.github.com>
2024-08-30 21:25:12 -0500

    Update db.js

--- server/db.js
+++ server/db.js
@@ -8,30 +8,5 @@ const pool = new Pool({
   ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false // Habilita SSL solo si se usa DATABASE_URL
 });
 
-// Función para verificar la conexión y consultar la versión del servidor
-const checkConnection = async () => {
-  let client;
-  try {
-    client = await pool.connect();
-    console.log('Conexión exitosa a la base de datos');
-    
-    // Consultar la versión del servidor PostgreSQL
-    const { rows } = await pool.query('SELECT version()');
-    console.log('Versión del servidor PostgreSQL:', rows[0].version);
-
-  } catch (err) {
-    console.error('Error al conectar a la base de datos:', err.message);
-  } finally {
-    // Liberar el cliente y cerrar el pool
-    if (client) client.release();
-    await pool.end();
-  }
-};
-
-// Verificar si la variable DATABASE_URL está configurada
-const urlDatabase = process.env.DATABASE_URL;
-
-
-// Ejecutar la verificación de conexión
-checkConnection();
-
+// Exporta el pool para que pueda ser usado en otros archivos
+module.exports = { pool };
