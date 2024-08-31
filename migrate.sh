880596c91ec6c169ac83bcccc2e3326e803f5059
MoonDog11 <123662244+MoonDog11@users.noreply.github.com>
2024-08-31 11:48:18 -0500

    Update migrate.sh

--- migrate.sh
+++ migrate.sh
@@ -2,105 +2,163 @@
 
 set -o pipefail
 
-section() {
-  echo "==== $1 ===="
-}
+# Definición de colores para salida en consola
+_GREEN="\033[0;32m"
+_BLUE="\033[0;34m"
+_MAGENTA="\033[0;35m"
+_RESET="\033[0m"
+_BOLD="\033[1m"
+_RED="\033[0;31m"
+_YELLOW="\033[1;33m"
 
+# Función para mostrar errores y salir
 error_exit() {
-  echo "Error: $1"
-  exit 1
+    printf "[ ${_RED}ERROR${_RESET} ] ${_RED}$1${_RESET}\n" >&2
+    exit 1
 }
 
-# Configuración de la base de datos
-export DB_USER=postgres
-export DB_PASSWORD=Jphv19840625*
-export DB_HOST=10.250.18.6
-export DB_PORT=5432
-export DB_DATABASE=nordeste_abogados_users_db
-export PLUGIN_URL=postgresql://postgres:Jphv19840625*@10.250.18.6:5432/nordeste_abogados_users_db
+# Función para mostrar secciones
+section() {
+  printf "${_RESET}\n"
+  echo "${_BOLD}${_BLUE}==== $1 ====${_RESET}"
+}
 
-# Configuración de Railway
-export RAILWAY_URL=postgresql://postgres:RoJuKhWPvLtbSQILdwueQPcKMGUuXMkE@viaduct.proxy.rlwy.net:56284/railway
+# Función para mostrar mensaje de éxito
+write_ok() {
+  echo "[$_GREEN OK $_RESET] $1"
+}
 
-# Define el directorio para el volcado
-dump_dir="plugin_dump"
-mkdir -p $dump_dir
+# Función para mostrar mensaje de información
+write_info() {
+  echo "[$_BLUE INFO $_RESET] $1"
+}
 
-# Función para verificar la conexión a la base de datos
-check_db_connection() {
-  section "Checking database connection"
+# Función para mostrar advertencia
+write_warn() {
+  echo "[$_YELLOW WARN $_RESET] $1"
+}
 
-  PGPASSWORD=$DB_PASSWORD psql -h $DB_HOST -p $DB_PORT -U $DB_USER -d $DB_DATABASE -c "\q" &> /dev/null
-  if [ $? -eq 0 ]; then
-    echo "Successfully connected to the database $DB_DATABASE at $DB_HOST:$DB_PORT"
-  else
-    error_exit "Failed to connect to the database $DB_DATABASE at $DB_HOST:$DB_PORT"
+# Captura de errores
+trap 'echo "An error occurred. Exiting..."; exit 1;' ERR
+
+# Encabezado del script
+printf "${_BOLD}${_MAGENTA}"
+echo "+-------------------------------------+"
+echo "|                                     |"
+echo "|  Railway Postgres Migration Script  |"
+echo "|                                     |"
+echo "+-------------------------------------+"
+printf "${_RESET}\n"
+
+# Validación de variables de entorno
+section "Validating environment variables"
+
+if [ -z "$PLUGIN_URL" ]; then
+    error_exit "PLUGIN_URL environment variable is not set."
+fi
+
+write_ok "PLUGIN_URL correctly set"
+
+if [ -z "$DATABASE_URL" ]; then
+    error_exit "DATABASE_URL environment variable is not set."
+fi
+
+write_ok "DATABASE_URL correctly set"
+
+# Extraer información de DATABASE_URL usando bash
+export DB_HOST=$(echo "$DATABASE_URL" | awk -F[@:] '{print $4}')
+export DB_PORT=$(echo "$DATABASE_URL" | awk -F[@:] '{print $5}')
+export DB_USER=$(echo "$DATABASE_URL" | awk -F[/@] '{print $4}')
+export DB_PASSWORD=$(echo "$DATABASE_URL" | awk -F[:] '{print $3}' | sed 's/@.*//')
+export DB_NAME=$(echo "$DATABASE_URL" | awk -F[/:] '{print $4}')
+
+section "Checking if DATABASE_URL is empty"
+
+query="SELECT count(*) FROM information_schema.tables WHERE table_schema NOT IN ('information_schema', 'pg_catalog');"
+table_count=$(PGPASSWORD="$DB_PASSWORD" psql -h "$DB_HOST" -U "$DB_USER" -p "$DB_PORT" -d "$DB_NAME" -t -A -c "$query")
+
+if [[ $table_count -eq 0 ]]; then
+  write_ok "The new database is empty. Proceeding with restore."
+else
+  echo "Table count: $table_count"
+  if [ -z "$OVERWRITE_DATABASE" ]; then
+    error_exit "The new database is not empty. Aborting migration.\nSet the OVERWRITE_DATABASE environment variable to overwrite the new database."
   fi
-}
+  write_warn "The new database is not empty. Found OVERWRITE_DATABASE environment variable. Proceeding with restore."
+fi
+
+dump_dir="plugin_dump"
+mkdir -p "$dump_dir"
 
 # Función para volcar la base de datos
 dump_database() {
-  local database=$1
-  local dump_file="$dump_dir/${database}_dump.sql"
+  local database="$1"
+  local dump_file="$dump_dir/$database.sql"
 
   section "Dumping database: $database"
 
-  echo "Dumping database from $PLUGIN_URL"
+  local base_url=$(echo "$PLUGIN_URL" | sed -E 's/(postgresql:\/\/[^:]+:[^@]+@[^:]+:[0-9]+)\/.*/\1/')
+  local db_url="${base_url}/${database}"
 
-  PGPASSWORD=$DB_PASSWORD pg_dump -h $DB_HOST -p $DB_PORT -U $DB_USER -d "$database" \
+  echo "Dumping database from $db_url"
+
+  PGPASSWORD="$DB_PASSWORD" pg_dump -h "$DB_HOST" -U "$DB_USER" -p "$DB_PORT" -d "$database" \
       --format=plain \
       --quote-all-identifiers \
       --no-tablespaces \
       --no-owner \
       --no-privileges \
-      --file=$dump_file || error_exit "Failed to dump database from $database."
+      --disable-triggers \
+      --file="$dump_file" || error_exit "Failed to dump database from $database."
 
-  echo "Successfully saved dump to $dump_file"
+  write_ok "Successfully saved dump to $dump_file"
+
+  dump_file_size=$(ls -lh "$dump_file" | awk '{print $5}')
+  write_info "Dump file size: $dump_file_size"
 }
 
-# Función para subir el volcado a Railway
-upload_dump() {
-  local dump_file=$1
-  local database=$2
+# Obtener listado de bases de datos a migrar
+databases=$(PGPASSWORD="$DB_PASSWORD" psql -h "$DB_HOST" -U "$DB_USER" -p "$DB_PORT" -d "$DB_NAME" -t -A -c "SELECT datname FROM pg_database WHERE datistemplate = false;")
+write_info "Found databases to migrate: $databases"
 
-  section "Uploading dump for database: $database"
-
-  PGPASSWORD=RoJuKhWPvLtbSQILdwueQPcKMGUuXMkE psql -h viaduct.proxy.rlwy.net -p 56284 -U postgres -d "$database" -f "$dump_file" || error_exit "Failed to upload dump to $database."
-  
-  echo "Successfully uploaded dump for $database"
-}
-
-# Inicializar el servidor
-initialize_server() {
-  section "Starting server"
-
-  # Aquí se incluirían las instrucciones para iniciar el servidor.
-  # Asegúrate de que cualquier comando o script que inicie el servidor se ejecuta aquí.
-  
-  # Ejemplo:
-  # npm start
-}
-
-section "Starting migration"
-
-# Verificar la conexión a la base de datos
-check_db_connection
-
-# Realizar el volcado
-databases=$(PGPASSWORD=$DB_PASSWORD psql -h $DB_HOST -p $DB_PORT -U $DB_USER -d "$DB_DATABASE" -t -A -c "SELECT datname FROM pg_database WHERE datistemplate = false;")
+# Iterar sobre cada base de datos para realizar el volcado
 for db in $databases; do
   dump_database "$db"
 done
 
-section "Uploading dumps to Railway"
+# Función para restaurar la base de datos desde el volcado
+restore_database() {
+  local database="$1"
 
-# Subir cada volcado a Railway
-for dump_file in $dump_dir/*_dump.sql; do
-  db_name=$(basename "$dump_file" _dump.sql)
-  upload_dump "$dump_file" "$db_name"
+  section "Restoring database: $database"
+
+  local base_url=$(echo "$DATABASE_URL" | sed -E 's/(postgresql:\/\/[^:]+:[^@]+@[^:]+:[0-9]+)\/.*/\1/')
+  local db_url="${base_url}/${database}"
+
+  local db_name=$(echo "$db_url" | sed -E 's/.*\/([^?]+).*/\1/')
+  local db_url_base=$(echo "$db_url" | sed -E 's/(.*)\/[^\/?]+/\1/')
+
+  if ! PGPASSWORD="$DB_PASSWORD" psql -h "$DB_HOST" -U "$DB_USER" -p "$DB_PORT" -d "$db_url_base" -tA -c "SELECT 1 FROM pg_database WHERE datname='$db_name'" | grep -q 1; then
+      write_ok "Database $db_name does not exist. Creating..."
+      PGPASSWORD="$DB_PASSWORD" psql -h "$DB_HOST" -U "$DB_USER" -p "$DB_PORT" -d "$db_url_base" -c "CREATE DATABASE \"$db_name\""
+  else
+      write_info "Database $db_name exists."
+  fi
+
+  PGPASSWORD="$DB_PASSWORD" psql -h "$DB_HOST" -U "$DB_USER" -p "$DB_PORT" -d "$db_url" -v ON_ERROR_STOP=1 --echo-errors \
+    -f "$dump_dir/$database.sql" || error_exit "Failed to restore database $database."
+  
+  write_ok "Successfully restored database $database from dump"
+}
+
+# Iterar sobre cada base de datos para restaurarla desde el volcado
+for db in $databases; do
+  restore_database "$db"
 done
 
-# Inicializar el servidor después de la migración
-initialize_server
+echo "Migration completed successfully."
 
-echo "Migration and server initialization completed successfully"
+# Iniciar el servidor (asumiendo que esta parte es para otro script o aplicación relacionada)
+section "Starting the server"
+# node app.js || error_exit "Failed to start the server."
+write_ok "Server started successfully."
