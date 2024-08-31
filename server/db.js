c166e1d83693edadb022616972759a296f8d051f
MoonDog11 <123662244+MoonDog11@users.noreply.github.com>
2024-08-30 19:48:43 -0500

    Update db.js

--- server/db.js
+++ server/db.js
@@ -4,12 +4,11 @@ const { Pool } = require('pg');
 
 // Configuración del Pool
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-  connectionString: process.env.DATABASE_URL,
+  user: 'postgres',
+  host: 'viaduct.proxy.rlwy.net',
+  database: 'railway',
+  password: 'RoJuKhWPvLtbSQILdwueQPcKMGUuXMkE',
+  port: 56284,
 });
 
 // Función para verificar la conexión y consultar la versión del servidor
